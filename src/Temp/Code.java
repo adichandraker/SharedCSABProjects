c50d6428d352e8b453fd67a2ee5dd04e434347b0
Ad1-C <chandraker@gmail.com>
2019-10-09 15:15:38 -0400

    Temp

---
+++ src/Temp/Code.java
@@ -0,0 +1,47 @@
+package Temp;
+
+public class Code {
+
+    /*String directory = System.getProperty("user.home");
+    String fileName = "sample.txt";
+    String absolutePath = directory + File.separator + fileName;
+
+    public static void main(String[] args) {
+        // write the content in file
+        try(
+                FileWriter fileWriter = new FileWriter(absolutePath))
+
+        {
+            String fileContent = "This is a sample text.";
+            fileWriter.write(fileContent);
+        } catch(
+                IOException e)
+
+        {
+            // exception handling
+        }
+
+        // read the content from file
+        try(
+                FileReader fileReader = new FileReader(absolutePath))
+
+        {
+            int ch = fileReader.read();
+            while (ch != -1) {
+                System.out.print((char) ch);
+                ch = fileReader.read();
+            }
+        } catch(
+                FileNotFoundException e)
+
+        {
+            // exception handling
+        } catch(
+                IOException e)
+
+        {
+            // exception handling
+        }
+    }*/
+
+}
