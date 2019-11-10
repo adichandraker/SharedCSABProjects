5ddadc25b7726f0e69a9f3bb32b154d83f44367c
Ad1-C <chandraker@gmail.com>
2019-11-09 21:27:55 -0500

    GUI update 2

---
+++ src/BattleShipLab/Coordinate.java
@@ -0,0 +1,19 @@
+package BattleShipLab;
+
+public class Coordinate {
+
+    private int xCoordinate;
+    private int yCoordinate;
+    private int stateOfCoordinate;
+
+    public Coordinate (int xCor, int yCor) {
+        xCoordinate = xCor;
+        yCoordinate = yCor;
+        stateOfCoordinate = 0;
+    }
+
+    public void changeState(int newState) {
+        stateOfCoordinate = newState;
+    }
+
+}
