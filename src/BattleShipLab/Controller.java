62c013c500567fb793796172d02eb0e725c6364c
Ad1-C <chandraker@gmail.com>
2019-11-09 21:24:26 -0500

    GUI update 2

--- src/BattleShipLab/Controller.java
+++ src/BattleShipLab/Controller.java
@@ -1,4 +1,13 @@
 package BattleShipLab;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class Controller {
+
+    @FXML
+    protected void handlePositionInput(ActionEvent event) {
+
+    }
+
 }
